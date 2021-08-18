c6fc70f62db7f20a32a70edab3442942e85b0ffe
Down <53242610+Down-s@users.noreply.github.com>
2021-08-18 20:20:36 +0100

    Update main.lua

--- UnityLua/main.lua
+++ UnityLua/main.lua
@@ -1,26 +1,4 @@
-local Player = Entity.Get("Player")
-Player:SetName("Lua Player")
-
-local Camera = Entity.Get("Main Camera")
-
-local function Movement()
-	local Speed = Input.GetKey(KeyCode.LeftShift) and 10 or 5
-	local Horizontal = Input.GetAxis("Horizontal")
-	local Vertical = Input.GetAxis("Vertical")
-
-	Player:Translate(Vector3(Horizontal, 0, Vertical) * Speed * Time.Delta())
-end
-
-Cursor.SetLockState(CursorLockMode.Locked)
-local function MouseLook()
-	local MouseX = Input.GetAxis("Mouse X");
-	local MouseY = Input.GetAxis("Mouse Y");
-
-	Camera:SetAng(Camera:GetAng() + Vector3(-MouseY, 0, 0))
-	Player:SetAng(Player:GetAng() + Vector3(0, MouseX, 0))
-end
+print("Loaded main.lua!")
 
 function UE:Update()
-	Movement()
-	MouseLook()
 end
